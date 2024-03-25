c4248c9b62fe73ca7c2e460cda4198d70ced0b35
Daniel Dias <danieldias.it98@gmail.com>
2024-03-25 14:54:57 +0000

    Change variables name

--- src/components/LibraryList.vue
+++ src/components/LibraryList.vue
@@ -23,11 +23,11 @@
                             <a href="#" class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white">{{ item.numberOfReviews }}</a>
                         </div>
                         <div class="flex items-center">
-                            <svg @click="handleClick( item )" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" :fill="item.bookFavorited ? 'red' : 'none'" stroke="red" class="w-6 h-6">
+                            <svg @click="handleClick( item )" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" :fill="item.isFavorited ? 'red' : 'none'" stroke="red" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                             </svg>
                             <p class="ms-2 text-sm font-bold text-gray-900 dark:text-white">
-                                {{ item.bookFavorited ? 'Remove from Favorite' : 'Add to Favorite' }}
+                                {{ item.isFavorited ? 'Remove from Favorite' : 'Add to Favorite' }}
                             </p>
                         </div>
                     </div>
@@ -78,7 +78,7 @@ export default{
     },
     methods: {
         handleClick( book ){
-            book.bookFavorited = !book.bookFavorited
+            book.isFavorited = !book.isFavorited
             // Use Vue.set or this.$set to make Vue detect the change
         },
         async fetchBooks(){
